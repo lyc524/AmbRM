ad3348bd550d7a76feb93ad91018eabbc5f98aec
kavilee <kavilee@163.com>
2016-05-26 18:12:50 +0800

    Sqlite操作类

--- app/src/main/java/com/lz/www/ambrm/model/User.java
+++ app/src/main/java/com/lz/www/ambrm/model/User.java
@@ -5,6 +5,16 @@ package com.lz.www.ambrm.model;
  */
 public class User {
     private int userID;
+    private String userName;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public int getUserID() {
         return userID;
@@ -12,5 +22,13 @@ public class User {
 
     public void setUserID(int userID) {
         this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
