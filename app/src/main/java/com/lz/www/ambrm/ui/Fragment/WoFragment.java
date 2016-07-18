f7359505a0f8a4697ac693729c91adfc167a05a5
kavilee <kavilee@163.com>
2016-07-18 18:39:32 +0800

    camera

--- app/src/main/java/com/lz/www/ambrm/ui/Fragment/WoFragment.java
+++ app/src/main/java/com/lz/www/ambrm/ui/Fragment/WoFragment.java
@@ -2,24 +2,37 @@ package com.lz.www.ambrm.ui.Fragment;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lz.www.ambrm.R;
+import com.lz.www.ambrm.ui.HeadPhotoActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class WoFragment extends Fragment {
+
+   TextView txtUpdateHead;
 
    @Nullable
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-      return inflater.inflate(R.layout.fragment_wo,container,false);
+      View view = inflater.inflate(R.layout.fragment_wo,container,false);
+      txtUpdateHead=(TextView)view.findViewById(R.id.tvMeHead);
+      txtUpdateHead.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            Intent it=new Intent(getActivity(), HeadPhotoActivity.class);
+            startActivity(it);
+            Toast.makeText(getActivity(),"clicked head",Toast.LENGTH_SHORT).show();
+         }
+      });
+
+      return view;
    }
 }
